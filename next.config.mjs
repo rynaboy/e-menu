eb4837633aae3a75304a2edee3c932fe47c09373
cana_trust <youremail@example.com>
2025-01-28 13:19:30 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import { hostname } from 'os';
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    images: {
+        
+        // domains: ['https://img.daisyui.com'],
+        remotePatterns:[
+            {
+                protocol: "https",
+                hostname: "**"
+            }
+        ]
+    },
+};
+
+export default nextConfig;
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='w-full p-32 '>
+        <p className='text-2xl text-nowrap font-battambong text-center'>ស្វែងរកពុំឃើញ...</p>
+    </div>
+  )
+}
